c397272b5917a680c332830eb57cd937fc6479ba
Phillipcodes <Phillipsauer00@gmail.com>
2024-06-01 23:59:51 +0200

    changed array

--- scrips.js
+++ scrips.js
@@ -23,7 +23,7 @@ async function fetchPokemonData() {
 }
 
 async function getPokemonData(pokemonAsJson) {
-  for (let i = 0; i < 151; i++) {
+  for (let i = 0; i < 12; i++) {
     let pokemonName = pokemonAsJson[i]["name"];
     try {
       let pokemonInfo = await fetch(
@@ -46,9 +46,16 @@ async function getPokemonData(pokemonAsJson) {
 function renderPokemonData(pokemonName, index, types) {
   let baseImgUrl = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${index}.png`;
   let uppercasePokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)
+  let pokeTypesHTML = "";
   let pokeTyp = types[0].type.name;
-  let UppercasePokeType = pokeTyp.charAt(0).toUpperCase() + pokeTyp.slice(1)
-  let pokeHtml =  renderPokemonDataHTML(uppercasePokemonName,baseImgUrl,UppercasePokeType,index,pokeTyp)
+  for(let j = 0; j < types.length; j++) {
+    let pokeTyp = types[j].type.name;
+    let UppercasePokeType = pokeTyp.charAt(0).toUpperCase() + pokeTyp.slice(1)
+    pokeTypesHTML += `<span class="poke-type ${pokeTyp}1 center-text font-bold">${UppercasePokeType}</span> `
+  }
+  
+  
+  let pokeHtml =  renderPokemonDataHTML(uppercasePokemonName,baseImgUrl,index,pokeTyp,pokeTypesHTML )
 
   document.getElementById("main_content").innerHTML += pokeHtml
 
@@ -56,7 +63,7 @@ function renderPokemonData(pokemonName, index, types) {
 }
 
 
-function renderPokemonDataHTML(pokeName, imageUrl, upperpokeType, id, pokeType) {
+function renderPokemonDataHTML(pokeName, imageUrl, id, pokeType,pokeTypesHTML ) {
   return `<div class="pokecard">
     <div id="poke_name" class="poke-name center-text ${pokeType}">
       <div class="display-row">
@@ -66,8 +73,8 @@ function renderPokemonDataHTML(pokeName, imageUrl, upperpokeType, id, pokeType) 
       <div id="poke_body" class="poke-body ${pokeType}">
         <img src="${imageUrl}" alt="">
       </div>
-      <div id="poke_type" class="poke-type center-text font-bold">
-        <span>${upperpokeType}</span>
+      <div id="poke_type" class=" center-text font-bold">
+      ${pokeTypesHTML}
       </div>
     </div>
   </div>`;
