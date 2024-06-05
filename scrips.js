1fc53bdcbc18e4335df146072e518d4e8423a839
Phillipcodes <Phillipsauer00@gmail.com>
2024-06-05 23:06:57 +0200

    created big card view

--- scrips.js
+++ scrips.js
@@ -1,6 +1,9 @@
 let loadedPokemon = []; // Globale Variable zum Speichern der geladenen Pokémon
 let pokemonPackage = 0;
 let pokemonImg = [];
+let pokeImgGif = [];
+
+
 
 
   function init() {
@@ -9,17 +12,17 @@ let pokemonImg = [];
 }
 
 async function getPokemonData() {
-  for (let i = pokemonPackage +1 ; i < pokemonPackage +25; i++) {
+  for (let i = pokemonPackage +1 ; i < pokemonPackage +13; i++) {
     try {
       let pokemonResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${i}`);
       let baseImgUrl =  await fetch (`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${i}.png`);
+      let baseGifUrl =  await fetch(`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/showdown/${i}.gif`);
       if (!pokemonResponse.ok) {
         throw new Error("Something went wrong.");
       }
-      
       let pokemonData = await pokemonResponse.json();
-      
       pokemonImg.push(baseImgUrl)
+      pokeImgGif.push(baseGifUrl)
       loadedPokemon.push(pokemonData); // Pokémon zur Liste der geladenen Pokémon hinzufügen
     } catch (error) {
       console.log("Es ist ein Fehler aufgetreten.");
@@ -27,7 +30,8 @@ async function getPokemonData() {
   }
 
   renderPokemonData()
-  pokemonPackage += 24;
+  
+  pokemonPackage += 13;
 
 }
 
@@ -50,16 +54,16 @@ function renderPokemonData() {
   }
 }
 
-function renderPokemonDataHTML(pokeName,  id, imgId, pokeTypesHTML,pokeTypeClass) {
+function renderPokemonDataHTML(pokeName,  id, noneFormattedId, pokeTypesHTML,pokeTypeClass) {
   
-  return `<div class="pokecard">
+  return `<div class="pokecard"  onclick="openSinglePokemonCard(${noneFormattedId})">
     <div id="poke_name" class="poke-name center-text ${pokeTypeClass} ">
       <div class="display-row">
         <h2>${pokeName}</h2>
         <div class="center-number">${id}</div>
       </div>
       <div id="poke_body" class="poke-body">
-        <img src="${pokemonImg[imgId].url}" alt="">
+        <img src="${pokemonImg[noneFormattedId].url}" alt="">
       </div>
       <div id="poke_type" class="center-text font-bold">
         ${pokeTypesHTML}
@@ -71,3 +75,82 @@ function renderPokemonDataHTML(pokeName,  id, imgId, pokeTypesHTML,pokeTypeClass
 function loadMorePokemon() {
   getPokemonData(); // Weitere Pokémon laden
 }
+
+
+
+function blurBackground () {
+  document.getElementById('single_card_background').classList.remove('d-none');
+  document.getElementById('main_content').classList.add('blur');
+  document.getElementById('single_pokemon_card').classList.remove('d-none')
+  document.getElementById('header').classList.add('blur');
+}
+
+function openSinglePokemonCard(id) {
+  document.getElementById('body').classList.add('no-scroll')
+  renderSinglePokemonCard(id)
+  blurBackground ()
+  
+}
+
+function renderSinglePokemonCard(id) {
+  let pokemonName = loadedPokemon[id].name;
+  let uppercasePokemonName = pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1); 
+  let pokeGif = pokeImgGif[id];
+  let pokeyTypeBG = loadedPokemon[id].types[0].type.name;
+
+ 
+
+  document.getElementById('single_pokemon_card_container').innerHTML = renderSinglePokemonCardHTML(id,uppercasePokemonName, pokeGif,pokeyTypeBG)
+}
+
+function renderSinglePokemonCardHTML(id ,uppercasePokemonName,pokeGif,pokeTypeBackground) {
+  return /*html*/`  
+  <div id="single_pokemon_card" class="single-pokemon-card d-none">
+  <div class="name-and-id-line ${pokeTypeBackground+"3"}">
+    <div class="center-name">${uppercasePokemonName}</div>
+    <div class="round-background">${id+1}</div>
+  </div>
+  <div class="single-pokemon-card-img ${pokeTypeBackground}"> <img src="${pokeGif.url}"></div>
+  <div class="pokemon-stats-container ${pokeTypeBackground+"2"}">
+    <div class="stat-line">
+      <div id="close_up_info_btn_1">Stats</div>
+      <div id="close_up_info_btn_2">Moves</div>
+      <div id="close_up_info_btn_3">Info</div>
+    </div>
+    <div class="seperator"></div>
+
+    <div id="poke_stats" class="stats-information">
+      <ul>
+        <li class="base-stats-item">
+          <span class="base-stat-name">HP</span>
+          <span class="base-stat-number">100</span>
+        </li>
+        <li class="base-stats-item">
+          <span class="base-stat-name">Attack</span>
+          <span class="base-stat-number">120</span>
+        </li>
+        <li class="base-stats-item">
+          <span class="base-stat-name">Defense</span>
+          <span class="base-stat-number">80</span>
+        </li>
+        <li class="base-stats-item">
+          <span class="base-stat-name">Special Attack</span>
+          <span class="base-stat-number">90</span>
+        </li>
+        <li class="base-stats-item">
+          <span class="base-stat-name">Special Defense</span>
+          <span class="base-stat-number">70</span>
+        </li>
+        <li class="base-stats-item">
+          <span class="base-stat-name">Speed</span>
+          <span class="base-stat-number">110</span>
+        </li>
+      </ul>
+    </div>
+  </div>
+</div>`
+  
+}
+
+
+  
